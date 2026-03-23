65555b724fbe5a337c3f5661fdb8152b1ce6eb29
Jaxxerson <wangairyan123@gmail.com>
2026-03-23 08:34:52 +0300

    Rename AddProduct function to addProduct

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 
-const AddProduct = () => {
+const addProduct = () => {
   const[productname, setProductname]=useState("")
   const[productdescription,setProductdescription]=useState("")
   const[productcost, setProductcost]=useState("")
